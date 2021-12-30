ef437554fa1a80ecf52177eca1e065cdba613a1d
Seryal Tuncer <79209799+Seryaltuncer@users.noreply.github.com>
2021-12-30 23:34:27 +0300

    Update StringClassEx1.java

--- StringClassEx1.java
+++ StringClassEx1.java
@@ -1,19 +1,26 @@
-package ymt111_week12_ex1;
+package StringExp ;
+
 public class StringClassEx1 {
     public static void main(String[] args) {
         String str = "YMT111 - Algorithm and Programming "
                 + "and Intro to Comp Sc." + 
                 " and Maths and Physics";
         
+       
         if(str.startsWith("ymt"))
             System.out.println("The string fits!");
+        
         if(str.endsWith("ing."))
             System.out.println("The string fits!");
+        
         if(str.regionMatches(true, 10, "algorithm", 0, 9))
             System.out.println("It matches!");
         
+        
+        
         int position = str.indexOf("and");
         int count = 0;
+       
         while(position != -1){
             ++count;
             position = str.indexOf("and", position+1);
